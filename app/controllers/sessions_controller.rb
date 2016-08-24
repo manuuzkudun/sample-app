4fa3dd7cee65475ea4e6fe8c8dafdffdf10b6ed5
Manu Uzkudun <manuuzkudun@hotmail.com>
2016-08-24 11:16:54 +0000

    Implements user remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      remember user
       redirect_to users_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
